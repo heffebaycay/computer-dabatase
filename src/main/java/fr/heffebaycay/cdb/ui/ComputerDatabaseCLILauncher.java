2af43ffa855337388c600035085614dec07f293e
Heffebaycay <heffebaycay@gmail.com>
2015-01-30 15:36:36 +0100

    Fixed CLI app not working

--- src/main/java/fr/heffebaycay/cdb/ui/ComputerDatabaseCLILauncher.java
+++ src/main/java/fr/heffebaycay/cdb/ui/ComputerDatabaseCLILauncher.java
@@ -2,12 +2,13 @@ package fr.heffebaycay.cdb.ui;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ComputerDatabaseCLILauncher {
 
   public static void main(String[] args) {
     
-    ApplicationContext ctx = new AnnotationConfigApplicationContext("fr.heffebaycay.cdb");
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("/applicationContextCLI.xml");
     
     ComputerDatabaseCLI cli = ctx.getBean(ComputerDatabaseCLI.class);
     cli.start(null);
