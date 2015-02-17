a8b5c4c29dbe32c8021c21fb1166ec8d140ccaf1
Heffebaycay <heffebaycay@gmail.com>
2015-02-17 16:43:35 +0100

    WebService now automatically detects and sets the locale

---
+++ computer-database-webservice/src/main/java/fr/heffebaycay/cdb/webservice/filter/LocaleDetectorFilter.java
@@ -0,0 +1,42 @@
+package fr.heffebaycay.cdb.webservice.filter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+
+@Provider
+public class LocaleDetectorFilter implements ContainerRequestFilter {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(LocaleDetectorFilter.class);
+  
+  @Override
+  public void filter(ContainerRequestContext requestContext) throws IOException {
+   
+    
+    // Fetch the list of acceptable languages
+    List<Locale> locales = requestContext.getAcceptableLanguages();
+    if(locales.size() > 0) {
+      // Since locales are ordered by weight, the first one is
+      // definitely the one we want to use
+      Locale locale = locales.get(0);
+      LOGGER.debug("filter(): Autodetected locale {}", locale);
+      LocaleContextHolder.setLocale(locale, true);
+    } else {
+      LOGGER.debug("filter(): Failed to detect any locale");
+    }
+    
+  }
+  
+  
+  
+
+}
